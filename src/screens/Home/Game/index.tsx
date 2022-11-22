f62d13afc90332ad31c18271739e661378bbd037
Pablo Henrique <pablohdev@outlook.com>
2022-11-22 08:05:10 -0300

    finished video2

--- src/screens/Home/Game/index.tsx
+++ src/screens/Home/Game/index.tsx
@@ -1,11 +1,29 @@
+import { useRef, useState, useEffect } from "react";
 import { Start } from "./Start";
+import { GameEngine } from "react-native-game-engine";
+
+import entities from "../../../entities";
+import { styles } from "./styles";
 
 const Game = () => {
+  const [running, setIsRunning] = useState(false);
+
+  const gameEngineRef = useRef();
+
   const handleOnStartGame = () => {
     // TODO:
   };
 
-  return <Start handleOnStartGame={handleOnStartGame} />;
+  // return <Start handleOnStartGame={handleOnStartGame} />;
+
+  return (
+    <GameEngine
+      ref={gameEngineRef}
+      running={running}
+      entities={entities()}
+      style={styles.engineContainer}
+    />
+  );
 };
 
 export { Game };
